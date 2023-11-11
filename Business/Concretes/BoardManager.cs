769bc141fc670220ac8eb8a044a169cf59db065c
“puyz” <puyzsoft@gmail.com>
2023-11-11 15:52:44 +0300

    tes

--- Business/Concretes/BoardManager.cs
+++ Business/Concretes/BoardManager.cs
@@ -9,7 +9,7 @@ namespace Business.Concretes
 {
     public class BoardManager : IBoardService
     {
-        // test2
+        // test23
         private readonly IBoardRepository _boardRepository;
         private readonly ITaskListService _taskListService;
         private readonly IBoardMemberService _boardMemberService;
